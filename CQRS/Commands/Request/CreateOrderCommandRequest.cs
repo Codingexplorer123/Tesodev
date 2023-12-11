864ad08112bc92e9b7e2decad3af7c1658b6d1a1
Berk Ozgurcan <129437360+Codingexplorer123@users.noreply.github.com>
2023-12-11 21:42:01 +0300

    CreateOrder Modellerine Data Annotation ile validasyon yapildi

--- CQRS/Commands/Request/CreateOrderCommandRequest.cs
+++ CQRS/Commands/Request/CreateOrderCommandRequest.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using TesodevCase.CQRS.Commands.Response;
 using TesodevCase.Entities;
 
@@ -7,9 +8,14 @@ namespace TesodevCase.CQRS.Commands.Request
 {
     public class CreateOrderCommandRequest : IRequest<CreateOrderCommandResponse>
     {
-        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="CustomerId girilmesi zorunludur.")]
         public Guid CustomerId { get; set; }
+        [Required(ErrorMessage ="Miktar girilmesi zorunludur.")]
+        [Range(1, 1000, ErrorMessage ="Miktar 0 dan buyu en fazla 1000 adet girilebilir")]
         public int Quantity { get; set; }
+        [Required(ErrorMessage ="Fiyat girilmek zorundadir")]
+        [Range(1,30000,ErrorMessage ="Fiyat 1 ile 30bin arasinda bir sayi olmalidir.")]
         public double Price { get; set; }
         public string Status { get; set; }
 
